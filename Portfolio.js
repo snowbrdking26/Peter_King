9a90a57c40dfd42514f9b5d8924dfb35ad4febc3
Peter King <30704554+snowbrdking26@users.noreply.github.com>
2017-10-01 18:56:28 -0400

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,15 @@
+/* exported myFunctionSticky */
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+
+function myFunctionSticky() {
+	"use strict";
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
