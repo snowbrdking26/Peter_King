e19ca5ae9ce3afbe930be5ae0727a9b7017a31e0
Peter King <30704554+snowbrdking26@users.noreply.github.com>
2018-01-19 22:08:30 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+
+// side nav menu
+// https://www.w3schools.com/howto/howto_js_sidenav.asp
+
+function openNav() {
+	"use strict";
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+	"use strict";
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function openResume() {
+	"use strict";
+    window.open("https://snowbrdking26.github.io/Peter_King/WDI_Resume.pdf");
+}
+
+function openBlog() {
+	"use strict";
+    window.open("https://snowbrdking26.github.io/First-Blog/BlogPK.html");
+}
+
+function openWebsite() {
+	"use strict";
+	window.open("https://snowbrdking26.github.io/First-Website/WebsitePK.html");
+}
+
+function openMemory() {
+	"use strict";
+    window.open("https://snowbrdking26.github.io/memory-game-improved/Memory-game-improved.html");
+}
+
+
+function openSkiBarn() {
+	"use strict";
+    window.open("https://dash.generalassemb.ly/snowboardking26/build-your-own-business-website");
+}
+
+function openAirTrafficGame() {
+	"use strict";
+    window.open("https://snowbrdking26.github.io/Tropical_Air_Traffic_Game/");
+}
+
+function openWineAtlas() {
+"use strict";
+    window.open("https://wine-atlas.herokuapp.com/wines/home");
+}
+
+function openTravelApp() {
+    "use strict";
+    window.open("https://travel-tripn.herokuapp.com/");
+}
+
+function openCinema() {
+    "use strict";
+    window.open("https://laventure-cinema-front.herokuapp.com/");
+}
